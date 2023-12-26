51dc6ac32e30959d0cf00029fe6f494cadebe7c1
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-12-26 14:28:51 -0500

    Added main logo

--- src/TopNavBar.jsx
+++ src/TopNavBar.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import SnapSiloLogo from './assets/main_logo.svg'
 import './TopNavBar.scss'
 
 function TopNavBar({ onNavBtnClick }) {
     return (
         <>
             <div id='div_7206'>
-                <h1>SnapSilo</h1>
+                <div id='div_9510'>
+                    <img src={SnapSiloLogo} alt="" />
+                    <h1>SnapSilo</h1>
+                </div>
                 <div id='div_5937'>
                     <button onClick={() => onNavBtnClick("sign in")}>Sign in</button>
                     <button id='button_1946' onClick={() => onNavBtnClick("sign up")}>Sign up</button>
