2c1a7c7ac8a5f8aba2c7912212ae8b7f685b978d
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-12-27 15:42:40 -0500

    Making responsive

--- src/TopNavBar.jsx
+++ src/TopNavBar.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import SnapSiloLogo from './assets/main_logo.svg'
 import './TopNavBar.scss'
+
+import SnapSiloLogo from './assets/main_logo.svg'
+import MenuIcon from './assets/menu.svg'
+import CloseIcon from './assets/close.svg'
 
 function TopNavBar({ onNavBtnClick }) {
     return (
         <>
             <div id='div_7206'>
-                <div id='div_9510'>
-                    <img src={SnapSiloLogo} alt="Main Logo" />
-                    <h1>SnapSilo</h1>
+                <div id='div_6815'>
+                    <div id='div_9510'>
+                        <img src={SnapSiloLogo} alt="Main Logo" />
+                        <h1>SnapSilo</h1>
+                    </div>
+                    <div id='div_8106'>
+                        <img src={MenuIcon} alt="Menu Icon" />
+                        <img src={CloseIcon} alt="Close Icon" />
+                    </div>
                 </div>
                 <div id='div_5937'>
                     <button onClick={() => onNavBtnClick("Sign in")}>Sign in</button>
