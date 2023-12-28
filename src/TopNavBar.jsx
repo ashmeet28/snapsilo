29f95b0ca95f4bbd9faada07a52bcf82fdafdfeb
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-12-27 23:06:42 -0500

    Added responsive nav bar

--- src/TopNavBar.jsx
+++ src/TopNavBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './TopNavBar.scss'
 
 import SnapSiloLogo from './assets/main_logo.svg'
@@ -6,26 +7,92 @@ import MenuIcon from './assets/menu.svg'
 import CloseIcon from './assets/close.svg'
 
 function TopNavBar({ onNavBtnClick }) {
-    return (
-        <>
-            <div id='div_7206'>
-                <div id='div_6815'>
-                    <div id='div_9510'>
-                        <img src={SnapSiloLogo} alt="Main Logo" />
-                        <h1>SnapSilo</h1>
+    const [isNavVisible, setIsNavVisible] = useState(false)
+
+    const [windowSize, setWindowSize] = useState([
+        window.innerWidth,
+        window.innerHeight,
+    ]);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowSize([window.innerWidth, window.innerHeight]);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
+    if (windowSize[0] > 600) {
+        return (
+            <>
+                <div id='div_7206'>
+                    <div id='div_6815'>
+                        <div id='div_9510'>
+                            <img src={SnapSiloLogo} alt="Main Logo" />
+                            <h1>SnapSilo</h1>
+                        </div>
+                        {/* <div id='div_8106'>
+                            <img src={MenuIcon} alt="Menu Icon" />
+                            <img src={CloseIcon} alt="Close Icon" />
+                        </div> */}
                     </div>
-                    <div id='div_8106'>
-                        <img src={MenuIcon} alt="Menu Icon" />
-                        <img src={CloseIcon} alt="Close Icon" />
+                    <div id='div_5937'>
+                        <button onClick={() => onNavBtnClick("Sign in")}>Sign in</button>
+                        <button onClick={() => onNavBtnClick("Sign up")}>Sign up</button>
                     </div>
                 </div>
-                <div id='div_5937'>
-                    <button onClick={() => onNavBtnClick("Sign in")}>Sign in</button>
-                    <button onClick={() => onNavBtnClick("Sign up")}>Sign up</button>
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    } else {
+        if (isNavVisible) {
+            return (
+                <>
+                    <div id='div_7206'>
+                        <div id='div_6815'>
+                            <div id='div_9510'>
+                                <img src={SnapSiloLogo} alt="Main Logo" />
+                                <h1>SnapSilo</h1>
+                            </div>
+                            <div id='div_8106'>
+                                {/* <img src={MenuIcon} alt="Menu Icon" /> */}
+                                <img src={CloseIcon} alt="Close Icon" onClick={() => setIsNavVisible(false)} />
+                            </div>
+                        </div>
+                        <div id='div_5937'>
+                            <button onClick={() => onNavBtnClick("Sign in")}>Sign in</button>
+                            <button onClick={() => onNavBtnClick("Sign up")}>Sign up</button>
+                        </div>
+                    </div>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <div id='div_7206'>
+                        <div id='div_6815'>
+                            <div id='div_9510'>
+                                <img src={SnapSiloLogo} alt="Main Logo" />
+                                <h1>SnapSilo</h1>
+                            </div>
+                            <div id='div_8106'>
+                                <img src={MenuIcon} alt="Menu Icon" onClick={() => setIsNavVisible(true)} />
+                                {/* <img src={CloseIcon} alt="Close Icon" /> */}
+                            </div>
+                        </div>
+                        {/* <div id='div_5937'>
+                            <button onClick={() => onNavBtnClick("Sign in")}>Sign in</button>
+                            <button onClick={() => onNavBtnClick("Sign up")}>Sign up</button>
+                        </div> */}
+                    </div>
+                </>
+            )
+        }
+    }
+
 }
 
 export default TopNavBar
